8598b3da45f500c89b8b36ac80b4a82088cfe443
Andre <ajparker000@gmail.com>
2019-04-18 14:52:16 -0700

    Recux Thunk

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,11 +1,24 @@
 // Splitting our reducere into 4 different files instead of having a single
 //file named Reducers.js that has all of the reducers on that 1 page 
+import * as ActionTypes from './ActionTypes';
 
-import { DISHES } from '../shared/dishes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes: []
+}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_DISHES:
+            return { ...state, isLoading: false, errMess: null, dishes: action.payload };
+
+        case ActionTypes.DISHES_LOADING:
+            return { ...state, isLoading: true, errMess: null, dishes: [] }
+
+        case ActionTypes.DISHES_FAILED:
+            return { ...state, isLoading: false, errMess: action.payload };
+
         default:
-          return state;
-      }
+            return state;
+    }
 };
